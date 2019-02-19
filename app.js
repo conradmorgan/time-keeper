103047ae5407bd80518f0dc392da172c10990d39
Conrad Morgan <conrad.adlai.morgan@gmail.com>
2019-02-18 17:58:19 -0800

    Added ability to edit fields, among other things.

--- app.js
+++ app.js
@@ -8,19 +8,101 @@ var excess = 0
 var timer
 var timing = false
 
-function main() {
+const fs = require('fs');
+
+function snapshot() {
+    return {
+        'expected': Number($('#expected').val()),
+        'tasks': tasks,
+        'total': total,
+        'excess': excess
+    }
+}
+
+function save() {
+    fs.writeFileSync('data.txt', JSON.stringify(snapshot()), 'utf-8')
+}
+
+function read() {
+    var obj = JSON.parse(fs.readFileSync('data.txt'))
+    $('#expected').val(obj.expected)
+    tasks = obj.tasks
+    total = obj.total
+    excess = obj.excess
+}
+
+function editLock() {
+    if ($('#editLock').html() === 'Edit') {
+        $('#editLock').html('Lock')
+        $('#start').prop('disabled', true)
+        $('#newSession').prop('disabled', true)
+        $('.stat').hide()
+        $('.stat-edit').show()
+        $('#tasks-edit').val(Math.round(tasks / 1000 / 60))
+        $('#total-edit').val(Math.round(total / 1000 / 60))
+        $('#excess-edit').val(Math.abs(Math.round(excess / 1000 / 60)))
+        if (excess < 0) {
+            $('#aheadBehind').val('behind')
+        } else {
+            $('#aheadBehind').val('ahead')
+        }
+    } else {
+        $('#start').prop('disabled', false)
+        $('#newSession').prop('disabled', false)
+        $('.stat').show()
+        $('.stat-edit').hide()
+        $('#editLock').html('Edit')
+        tasks = Number($('#tasks-edit').val())
+        total = Number($('#total-edit').val()) * 60 * 1000
+        excess = ($('#aheadBehind').val() == "ahead" ? 1 : -1) * Number($('#excess-edit').val()) * 60 * 1000
+        $('#tasks').html(tasks)
+        $('#total').html(Math.round(total / 1000 / 60))
+        setExcess(excess)
+        save()
+    }
+}
+
+function newSession() {
+    remote.getCurrentWindow().confirmation("Do you really want to start a new session?",
+        (response) => {
+            if (response === 0) {
+                stop()
+                tasks = 0
+                total = 0
+                excess = 0
+                save()
+                init()
+            }
+    })
+}
+
+function setExcess(excess) {
+    if (timing) {
+        $('#excess').html(Math.abs(Math.round(excess / 1000 / 60 * 100)/100))
+    } else {
+        $('#excess').html(Math.abs(Math.round(excess / 1000 / 60)))
+    }
+    $('#excessDir').html(excess < 0 ? "behind" : "ahead")
+    $('#excessDir').css('color', excess < 0 ? 'red' : 'blue')
+}
+
+
+function init() {
+    read()
+    $('#start').prop('disabled', false)
     $('#stop').prop('disabled', true)
     $('#next').prop('disabled', true)
     $('#tasks').html(tasks)
-    $('#total').html(Math.round(total / 1000 / 60) + " minutes")
-    $('#excess').html(Math.round(excess / 1000 / 60) + " minutes")
+    $('#total').html(Math.round(total / 1000 / 60))
+    setExcess(excess)
+    $('.stat-edit').hide()
 }
 
 function update() {
     var d = Date.now() - beginTask + total
     var r = 1000 * 60 * Number($('#expected').prop('value')) - d + excess
-    $('#total').html(Math.round(d / 1000 / 60 * 100)/100 + " minutes")
-    $('#excess').html(Math.round(r / 1000 / 60 * 100)/100 + " minutes")
+    $('#total').html(Math.round(d / 1000 / 60 * 100)/100)
+    setExcess(r)
 }
 
 function start() {
@@ -31,6 +113,7 @@ function start() {
     $('#next').prop('disabled', false)
     timer = setInterval(update, 10)
     remote.getCurrentWindow().greenTray()
+    $('#editLock').prop('disabled', true)
 }
 
 function stop() {
@@ -42,12 +125,14 @@ function stop() {
     excess += rt - d
     clearInterval(timer)
     $('#tasks').html(tasks)
-    $('#total').html(Math.round(total / 1000 / 60) + " minutes")
-    $('#excess').html(Math.round(excess / 1000 / 60) + " minutes")
+    $('#total').html(Math.round(total / 1000 / 60))
+    setExcess(excess)
     $('#start').prop('disabled', false)
     $('#stop').prop('disabled', true)
     $('#next').prop('disabled', true)
     remote.getCurrentWindow().redTray()
+    save()
+    $('#editLock').prop('disabled', false)
 }
 
 function next() {
--- main.js
+++ main.js
@@ -1,10 +1,16 @@
-const {app, globalShortcut, BrowserWindow, Tray, ipcMain} = require('electron')
+const {app, globalShortcut, BrowserWindow, Tray, ipcMain, dialog} = require('electron')
 const {autoUpdater} = require("electron-updater")
 
 let win
 
 function createDefaultWindow() {
-    win = new BrowserWindow({width: 400, height: 600, resizable: false})
+    win = new BrowserWindow({width: 400, height: 500, resizable: false})
+    win.confirmation = function(message, callback) {
+        dialog.showMessageBox(win, {
+            buttons: ["Yes", "Cancel"],
+            message: message
+        }, callback)
+    }
     win.redTray = function() {
         win.setOverlayIcon(app.getAppPath() + '/assets/red-icon.png', 'Time Keeper')
     }
@@ -15,27 +21,37 @@ function createDefaultWindow() {
     win.loadFile('index.html')
     const ret = globalShortcut.register('Alt+W', () => {win.webContents.executeJavaScript('next()')})
     win.on('closed', () => app.quit())
-    return win
 }
-app.on('ready', function() {
-  createDefaultWindow()
-    autoUpdater.checkForUpdates()
-})
+
 autoUpdater.on('update-downloaded', (info) => {
     win.webContents.send('updateReady')
 })
-ipcMain.on("quitAndInstall", (event, arg) => {
+
+autoUpdater.on('download-progress', (progress) => {
+    win.webContents.send("downloadProgress", progress.percent)
+})
+
+ipcMain.on('quitAndInstall', (event, arg) => {
     autoUpdater.quitAndInstall()
 })
+
+app.on('ready', function() {
+    createDefaultWindow()
+    autoUpdater.checkForUpdates()
+})
+
 app.on('will-quit', () => {
   globalShortcut.unregisterAll()
 })
+
 app.on('window-all-closed', () => {
   globalShortcut.unregisterAll()
 })
+
 app.on('before-quit', () => {
   globalShortcut.unregisterAll()
 })
+
 app.on('quit', () => {
   globalShortcut.unregisterAll()
 })
